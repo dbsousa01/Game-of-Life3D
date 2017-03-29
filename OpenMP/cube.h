84c5edfa0b10f155c7937567e14bc7fc0e820ffa
Nuno Venturinha <nuno.venturinha.f@tecnico.ulisboa.pt>
2017-03-29 22:29:32 +0100

    Add files via upload

---
+++ OpenMP/cube.h
@@ -0,0 +1,12 @@
+#ifndef CUBE_H
+#define CUBE_H
+
+#include "node.h"
+
+struct node *** create_cube(int size);
+void destroy_cube(struct node ***cube, int size);
+void determine_next_generation(struct node ***cube, int size);
+void mark_neighbors(omp_lock_t **locks, struct node ***cube, int size);
+void purge(struct node ***cube, int size);
+
+#endif
---
+++ OpenMP/life3d-omp.c
@@ -0,0 +1,69 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "defs.h"
+#include "cube.h"
+#include "node.h"
+#include "util.h"
+
+int main(int argc, char *argv[])
+{
+    omp_lock_t **locks;
+
+    FILE *input_fd;
+
+    struct node ***cube;
+
+    char buffer[BUFFER_SIZE];
+    char *input_filename;
+
+    int iterations;
+    int size;
+    int x, y, z;
+
+    /* ADD ARGUMENT NUMBER AND TYPE CHECKING, ETC */
+    input_filename = (char *) malloc((strlen(argv[1]) * sizeof(char)) + 1);
+    strcpy(input_filename, argv[1]);
+    iterations = atoi(argv[2]);
+
+    /* OPEN INPUT */
+    input_fd = fopen(input_filename, "r");
+    free(input_filename);
+
+    /* READ SIZE */
+    fgets(buffer,BUFFER_SIZE,input_fd);
+    sscanf(buffer,"%d", &size);
+
+    /* ALLOC CUBE */
+    cube = create_cube(size);
+    locks = create_locks(size);
+
+    /* READ INPUT COORDINATES */
+    while(fgets(buffer,BUFFER_SIZE,input_fd) != NULL)
+    {
+        sscanf(buffer,"%d %d %d", &x, &y, &z);
+        add_node(&(cube[x][y]), z, ALIVE, 0);
+    }
+    fclose(input_fd);
+
+    /* PROCESS THE REQUIRED NUMBER OF ITERATIONS */
+    #pragma omp parallel
+    {
+        while(iterations > 0)
+        {
+            mark_neighbors(locks, cube, size);
+            determine_next_generation(cube, size);
+            purge(cube, size);
+            #pragma omp single
+            iterations--;
+        }
+    }
+
+    /* WRITE OUTPUT */
+    write_output(cube, size);
+    /* FREE MEMORY */
+    destroy_cube(cube, size);
+
+    return 1;
+}
